9e6686f4d9d56de096d12c9f4dc9315351f9f7db
CaptainHook16 <45101692+CaptainHook16@users.noreply.github.com>
2019-06-07 22:02:19 +0300

    Update removeSpaces.cpp

--- Strings/removeSpaces.cpp
+++ Strings/removeSpaces.cpp
@@ -1,50 +1,71 @@
 #include<stdio.h>
 #include <iostream> 
 using namespace std; 
-
-void removeSpaces(string &str){
-    int i=0;
-    int pos=0;
-    int n = str.length();
-    
-    while(i<n && str[i]==' '){
-        i++;
-    }
-    
-    bool space_found=false;
-    while(i<n)
-    {
-        if(str[i] !=' ')
-        {
-            if(pos-1>=0 && str[pos-1] == ' ' && 
-            (str[i]==',' || str[i] == '.' || str[i]=='?'))
-            {
-                str[pos-1] = str[i];
-                i++;
+void removeSpaces(string& s)
+{
+    int pos = 0;
+    int n = s.length();
+    for(int i=0;i<n;i++){
+        if((s[i]=='.' || s[i]==',' || s[i]=='?') && pos>=1 && s[i-1]==' '){
+            s[pos] = s[i];
+            pos++;
+            
+        }else if(s[i] != ' '){
+            if(pos != 0){
+                s[pos++] = ' ';
             }
-            else{
-                str[pos] = str[i];
-                i++;
-                pos++;
+            int j = i;
+            while(j<n && s[j]!=' '){
+                s[pos++] = s[j++];
             }
-            space_found = false;
-        }
-        else if(str[i]== ' '){
-            if(space_found == false){
-                space_found = true;
-                str[pos]=' ';
-                pos++;
-            }
-            i++;
+            i=j;
         }
     }
-    //now we need to delete the rest 
-    str.erase(str.begin() + pos , str.end());
-    cout<<pos;
-    
-    
-    
+    s.erase(s.begin()+pos,s.end());
 }
+// void removeSpaces(string &str){
+//     int i=0;
+//     int pos=0;
+//     int n = str.length();
+    
+//     while(i<n && str[i]==' '){
+//         i++;
+//     }
+    
+//     bool space_found=false;
+//     while(i<n)
+//     {
+//         if(str[i] !=' ')
+//         {
+//             if(pos-1>=0 && str[pos-1] == ' ' && 
+//             (str[i]==',' || str[i] == '.' || str[i]=='?'))
+//             {
+//                 str[pos-1] = str[i];
+//                 i++;
+//             }
+//             else{
+//                 str[pos] = str[i];
+//                 i++;
+//                 pos++;
+//             }
+//             space_found = false;
+//         }
+//         else if(str[i]== ' '){
+//             if(space_found == false){
+//                 space_found = true;
+//                 str[pos]=' ';
+//                 pos++;
+//             }
+//             i++;
+//         }
+//     }
+//     //now we need to delete the rest 
+//     str.erase(str.begin() + pos , str.end());
+//     cout<<pos;
+    
+    
+    
+// }
 
 int main()
 {
